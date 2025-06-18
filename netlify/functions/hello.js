1f2e78bf4c5263814c7e54f3764085045c3fa95a
Dustin Wesely <weselyd@csp.edu>
2025-06-17 23:20:47 -0500

    Added Access-Control-Allow-Origin header to hello.js function

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -2,6 +2,11 @@ exports.handler = async event => {
     const subject = event.queryStringParameters.name || 'World'
     return {
         statusCode: 200,
+            headers: {
+                "Access-Control-Allow-Origin": "*", // Or specify your domain instead of '*'
+                "Access-Control-Allow-Headers": "Content-Type, Authorization",
+                "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+            },
         body: `Hello ${subject}!`,
     }
 }
