0f7fb0787a1a875bcabecde6e12859f6029662ae
Dustin Wesely <weselyd@csp.edu>
2025-06-18 15:41:45 -0500

    Change all CORS headers to allow all origins for testing purposes

--- netlify/functions/caradvice.js
+++ netlify/functions/caradvice.js
@@ -36,7 +36,7 @@ exports.handler = async (event, context) => {
     return {
       statusCode: 200,
       headers: {
-        'Access-Control-Allow-Origin': 'https://weselyd.github.io', // or your specific origin
+        'Access-Control-Allow-Origin': '*', // or https://weselyd.github.io
         'Access-Control-Allow-Headers': 'Content-Type, Authorization',
         'Access-Control-Allow-Methods': 'POST, OPTIONS',
       },
--- netlify/functions/openai.js
+++ netlify/functions/openai.js
@@ -39,7 +39,7 @@ exports.handler = async (event, context) => {
     return {
       statusCode: 200,
       headers: {
-        'Access-Control-Allow-Origin': 'https://weselyd.github.io', // or your specific origin
+        'Access-Control-Allow-Origin': '*', // https://weselyd.github.io
         'Access-Control-Allow-Headers': 'Content-Type, Authorization',
         'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
       },
