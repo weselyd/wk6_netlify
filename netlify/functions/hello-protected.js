b61c4f490159f145eb4e4f39e352a93e7ab17b0c
Dustin Wesely <weselyd@csp.edu>
2025-06-18 00:55:25 -0500

    Beginning to iterate on protected hello world API.

---
+++ netlify/functions/hello-protected.js
@@ -0,0 +1,12 @@
+exports.handler = async event => {
+    const subject = event.queryStringParameters.name || 'World'
+    return {
+        statusCode: 200,
+            headers: {
+                "Access-Control-Allow-Origin": "*", // Or specify your domain instead of '*'
+                "Access-Control-Allow-Headers": "Content-Type, Authorization",
+                "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+            },
+        body: JSON.stringify({ message: "Hello World!" }),
+    }
+}
